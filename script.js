725ee8f1ae305707115015c66f2061d653cf688b
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-03 19:15:33 +0200

    Added missing console statement in script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log('Welcome to the Remote Work Toolkit!');
 // Initialize the following functions for the dashboard.
+
+function initializeDashboard() {
+    // Initialize components here
+}
+
+initializeDashboard();
